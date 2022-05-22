dd91e6dd75763a7ffc6e27d65c584959e302026a
Ernest <ernesttancw@gmail.com>
2022-05-22 11:24:47 -0700

    Added functionality to check if all ships on board are sunk

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -4,6 +4,7 @@ function Gameboard() {
   const initBoard = () => new Array(10).fill(0).map(() => new Array(10).fill(0));
 
   const board = initBoard();
+  const placedShips = [];
 
   const placeShip = (hPos, vPos, ship, dir) => {
     /*
@@ -24,15 +25,38 @@ function Gameboard() {
     board[0][0 + i] = 's'
 
     */
+    let shipPos = 0;
     // Place ship horizontally
     if (dir === 'h') {
       for (let i = 0; i < ship.length; i++) {
-        board[hPos][vPos + i] = ship;
+        placedShips.push(ship);
+        board[hPos][vPos + i] = { ship, shipPos };
+        shipPos++;
+      }
+    }
+
+    if (dir === 'v') {
+      for (let i = 0; i < ship.length; i++) {
+        placedShips.push(ship);
+        board[hPos + i][vPos] = { ship, shipPos };
+        shipPos++;
       }
     }
   };
 
-  return { initBoard, placeShip, board };
+  const receiveAttack = (hPos, vPos) => {
+    if (board[hPos][vPos].ship) {
+      board[hPos][vPos].ship.hit(board[hPos][vPos].shipPos);
+    } else {
+      // Register a miss
+    }
+  };
+
+  const allShipsSunk = () => placedShips.every((ship) => ship.isSunk() === true);
+
+  return {
+    initBoard, placeShip, receiveAttack, board, allShipsSunk,
+  };
 }
 
 export default Gameboard;
--- src/tests/gameboard.spec.js
+++ src/tests/gameboard.spec.js
@@ -17,7 +17,39 @@ describe('Gameboard', () => {
 
   it('places a ship of length 3 horizontally on the board', () => {
     board.placeShip(0, 0, ship, 'h');
-    expect(board.board[0][0] && board.board[0][1] && board.board[0][2]).toBe(ship);
+    expect(board.board[0][0]).toStrictEqual({ ship, shipPos: 0 });
+    expect(board.board[0][1]).toStrictEqual({ ship, shipPos: 1 });
+    expect(board.board[0][2]).toStrictEqual({ ship, shipPos: 2 });
     // expect(board.board[0]).toEqual(['s', 's', 's', 0, 0, 0, 0, 0, 0, 0]);
+  });
+
+  it('plaeces a ship of length 3 vertically on the board', () => {
+    board.placeShip(0, 0, ship, 'v');
+    expect(board.board[0][0]).toStrictEqual({ ship, shipPos: 0 });
+    expect(board.board[1][0]).toStrictEqual({ ship, shipPos: 1 });
+    expect(board.board[2][0]).toStrictEqual({ ship, shipPos: 2 });
+  });
+
+  it('allows a ship to receive an attack', () => {
+    board.placeShip(0, 0, ship, 'h');
+    board.receiveAttack(0, 0, ship);
+    expect(ship.ship[0]).toEqual(1);
+  });
+
+  it('sinks a ship after receiving attacks', () => {
+    board.placeShip(0, 0, ship, 'h');
+    board.receiveAttack(0, 0, ship);
+    board.receiveAttack(0, 1, ship);
+    board.receiveAttack(0, 2, ship);
+    console.log(board.board);
+    expect(ship.isSunk()).toEqual(true);
+  });
+
+  it('verifies that all ships are sunk', () => {
+    board.placeShip(0, 0, ship, 'h');
+    board.receiveAttack(0, 0, ship);
+    board.receiveAttack(0, 1, ship);
+    board.receiveAttack(0, 2, ship);
+    expect(board.allShipsSunk()).toEqual(true);
   });
 });
